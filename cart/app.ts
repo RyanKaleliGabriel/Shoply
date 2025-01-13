a6b8da25c38e6f25a59dc7747249d481cec98773
RyanKaleliGabriel <ryang9241@gmail.com>
2025-01-13 16:11:43 +0300

    Cart service

--- cart/app.ts
+++ cart/app.ts
@@ -1,5 +1,28 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-const app = express()
+import cartRoute from "./routes/cartRoute";
+import AppError from "./utils/appError";
+import globalErrorHandler from "./controllers/errorController";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/cart", cartRoute);
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  return next(
+    new AppError(`Could not find ${req.originalUrl} in this server`, 401)
+  );
+});
+
+app.use(globalErrorHandler);
 
 export default app;
