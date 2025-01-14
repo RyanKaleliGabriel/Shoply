cee91943de126393e243485bd3ba804d9b6127bf
RyanKaleliGabriel <ryang9241@gmail.com>
2025-01-14 12:57:26 +0300

    Filter, pagination, sort and limit utility

--- product/utils/ApiFeatures.ts
+++ product/utils/ApiFeatures.ts
@@ -1,56 +1,91 @@
-// interface QueryStr {
-//   [key: string]: any;
-// }
+interface QueryStr {
+  [key: string]: any;
+}
 
-// class APIfeatures {
-//   query: any;
-//   queryString: QueryStr;
-//   values: any[];
+class APIfeatures {
+  query: any;
+  queryString: QueryStr;
+  values: any[];
 
+  constructor(query: any, queryString: QueryStr) {
+    this.query = query;
+    this.queryString = queryString;
+    this.values = [];
+  }
 
-//   constructor(query: any, queryString: QueryStr) {
-//     this.query = query;
-//     this.queryString = queryString;
-//   }
+  filter() {
+    const queryObj: QueryStr = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-//   //   filter() {
-//   //     const queryObj: QueryStr = { ...this.queryString };
-//   //     const excludedFields = ["page", "sort", "limit", "fields"];
+    const operatorsMap: { [key: string]: string } = {
+      gte: ">=",
+      gt: ">",
+      lte: "<=",
+      lt: "<",
+    };
 
-//   //     excludedFields.forEach((el) => delete queryObj[el]);
+    const filters: string[] = [];
+    let index = 1;
 
-//   //     let queryStr = JSON.stringify(queryObj)
+    for (const key in queryObj) {
+      const value = queryObj[key];
 
-//   //   }
+      // Check if value is an object (e.g., { gte: '100' })
+      if (typeof value === "object" && value !== null) {
+        for (const operatorKey in value) {
+          const operator = operatorsMap[operatorKey];
+          if (operator) {
+            filters.push(`${key} ${operator} $${index}`);
+            this.values.push(value[operatorKey]);
+            index++;
+          }
+        }
+      } else {
+        // Handle simple key-value pairs (e.g., `name=John`)
+        filters.push(`${key} = $${index}`);
+        this.values.push(value);
+        index++;
+      }
+    }
 
-//   // Sort rows by the specified fields
-//   sort() {
-//     if (this.queryString.sort) {
-//       const sortBy = this.queryString.sort.split(",").join(", ");
-//       this.query += ` ORDER BY ${sortBy}`;
-//     }
-//     return this;
-//   }
+    if (filters.length > 0) {
+      this.query += ` WHERE ${filters.join(" AND ")}`;
+    }
 
-//   // Select specific fields
-//   limitFields() {
-//     if (this.queryString.fields) {
-//       const fields = this.queryString.fields.split(",").join(", ");
-//       this.query = this.query.replace("*", fields);
-//     }
-//     return globalThis;
-//   }
+    return this;
+  }
 
-//   paginate() {
-//     const page = parseInt(this.queryString.page, 10) || 1;
-//     const limit = parseInt(this.queryString.limit, 10) || 10;
-//     const offset = (page - 1) * limit;
+  // Sort rows by the specified fields
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(", ");
+      this.query += ` ORDER BY ${sortBy}`;
+    }
+    return this;
+  }
 
-//     this.query += ` LIMIT $${this.values.length + 1} OFFSET $${
-//       this.values.length + 2
-//     }`;
-//     this.values.push(limit, offset);
+  // Select specific fields
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(", ");
+      this.query = this.query.replace("*", fields);
+    }
+    return this;
+  }
 
-//     return this;
-//   }
-// }
+  paginate() {
+    const page = parseInt(this.queryString.page, 10) || 1;
+    const limit = parseInt(this.queryString.limit, 10) || 10;
+    // OFFSET -  Specifies the number of rows to skip before starting to return rows.
+    const offset = (page - 1) * limit;
+    //LIMIT - Specifies the maximum number of rows to return.
+    this.query += ` LIMIT $${this.values.length + 1} OFFSET $${
+      this.values.length + 2
+    }`;
+    this.values.push(limit, offset);
+    return this;
+  }
+}
+
+export default APIfeatures;
